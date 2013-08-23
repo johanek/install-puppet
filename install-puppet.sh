6943533b0396730887d4ab2161e6bd376f5468a1
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2013-08-23 16:22:19 +0100

    Puppet Install script

---
+++ install-puppet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yum -y install http://mirrors.coreix.net/fedora-epel/6/i386/epel-release-6-8.noarch.rpm http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+yum -y install puppet
